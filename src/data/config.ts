43893382f39ec8ce4105cb90593bd88de6f096fc
kumarvs3537 <kumarvs3537@gmail.com>
2026-04-09 12:02:07 +0000

    Fixes: Landing page polished with adding few sections and remove few, Header changed with jumbo menu, Footer changed to standads

---
+++ src/data/config.ts
@@ -0,0 +1,26 @@
+export const siteConfig = {
+    name: 'Expert Guru',
+    tagline: 'Your Pathway to Mastery',
+    email: 'info@expertguru.net',
+    phone: '',
+    address: 'Plot No 2/P and 3/P, Venkusa Estates, Kowkoor, Secunderabad, Telangana-500010, India',
+  
+    // TMS Application URLs
+    // Currently pointing to test instance. 
+    // Production: switch to app.expertguru.net (or chosen subdomain)
+    app: {
+      baseUrl: 'https://dgtutor.netlify.app',
+      login: 'https://dgtutor.netlify.app/login',
+      register: 'https://dgtutor.netlify.app/register',
+      bookDemo: 'https://dgtutor.netlify.app/register',
+      parentPortal: 'https://dgtutor.netlify.app/login',
+    },
+  
+    social: {
+      instagram: 'https://www.instagram.com/expertguru_/',
+      facebook: 'https://www.facebook.com/',
+      youtube: '',
+      linkedin: '',
+    },
+  };
+  
